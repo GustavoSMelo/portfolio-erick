abe52429865c3c967db520decc752ce3fe14640f
GustavoSMelo <gsantos15569@gmail.com>
2025-12-29 22:09:03 -0300

    :sparkles: create projects content

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,12 @@ import 'primeicons/primeicons.css';
 import Navbar from './components/Navbar/Navbar.vue';
 import Introduction from './components/Introduction/Introduction.vue';
 import About from './components/About/About.vue';
+import Projects from './components/Projects/Projects.vue';
 
 </script>
 <template>
     <Navbar />
     <Introduction />
     <About />
+    <Projects />
 </template>
---
+++ src/components/Projects/Projects.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="projectsContainer">
+        <h1>Projetos</h1>
+
+        <ul class="projectsList">
+            <li class="projectItem">
+                <h1>Automacao</h1>
+                <video controls>
+                    <source src="../../assets/videoBoilerplate.mp4" type="video/mp4" />
+                </video>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti sequi quasi, eos voluptas quaerat,
+                    neque dolores culpa blanditiis aut cupiditate iure accusantium numquam. Similique obcaecati placeat
+                    magni esse accusamus dolore.
+                </p>
+
+                <button type="button">Baixar</button>
+            </li>
+                        <li class="projectItem">
+                <h1>Automacao</h1>
+                <video controls>
+                    <source src="../../assets/videoBoilerplate.mp4" type="video/mp4" />
+                </video>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti sequi quasi, eos voluptas quaerat,
+                    neque dolores culpa blanditiis aut cupiditate iure accusantium numquam. Similique obcaecati placeat
+                    magni esse accusamus dolore.
+                </p>
+
+                <button type="button">Baixar</button>
+            </li>
+                        <li class="projectItem">
+                <h1>Automacao</h1>
+                <video controls>
+                    <source src="../../assets/videoBoilerplate.mp4" type="video/mp4" />
+                </video>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti sequi quasi, eos voluptas quaerat,
+                    neque dolores culpa blanditiis aut cupiditate iure accusantium numquam. Similique obcaecati placeat
+                    magni esse accusamus dolore.
+                </p>
+
+                <button type="button">Baixar</button>
+            </li>
+        </ul>
+    </div>
+</template>
+<style lang="scss" src="./projects.style.scss" />
